464ae2b29347219cc4fbf17c7a5a3030ebf4d270
gwakgh <gwakggh304@gmail.com>
2025-05-27 20:28:17 +0900

    fix: 코드 수정

--- OOP-Summary/main.cpp
+++ OOP-Summary/main.cpp
@@ -3,91 +3,73 @@
 #include <string>
 #include <vector>
 
+using namespace std;
+
 class Animal {
-	char sound[20];
 public:
-	void setSound(const char* str);
-	void makeSound();
+	virtual void makeSound() { cout << "悼拱家府" << endl; }
 };
-class Zoo {
-private:
-	std::vector<Animal*> animals;
+
+class Dog : public Animal {
 public:
-	void addAnimal(Animal* animal);
+	void makeSound() override { cout << "港港" << endl; }
+};
+class Cat : public Animal {
+public:
+	void makeSound() override { cout << "具克" << endl; }
+};
+class Cow : public Animal {
+public:
+	void makeSound() override { cout << "澜皋" << endl; }
+};
+class Zoo{
+private:
+	Animal* animals[10];
+public:
+	void addAnimal(Animal* animal, int index);
 	void performActions();
 	~Zoo();
 };
 Animal* createRandomAnimal();
-
 int main() {
-	Animal Dog, Cat, Cow;
-	Animal* animals[] = { &Dog, &Cat, &Cow };
+	Dog dog;
+	Cat cat;
+	Cow cow;
+	Animal* animals[3] = { &dog, &cat, &cow };
 	Zoo zoo;
 
-	srand(time(NULL));
-
-	Dog.setSound("港港");
-	Cat.setSound("具克");
-	Cow.setSound("澜皋");
-
-	for (int i = 0; i < 3; i++) {
+	for(int i = 0; i < 3; i++) {
 		animals[i]->makeSound();
 	}
-	std::cout << std::endl;
 
-	for (int i = 0; i < 10; i++) {
-		std::cout << i;
-		zoo.addAnimal(createRandomAnimal());
-		zoo.performActions();
+	for(int i = 0; i < 10; i++) {
+		zoo.addAnimal(createRandomAnimal(), i);
 	}
-}
 
+	zoo.performActions();
+}
 Animal* createRandomAnimal() {
-	int r = rand() % 3;
-	Animal* animal = new Animal();
-
-	switch (r) {
-	case 0:
-		animal->setSound("港港");
-		return animal;
-	case 1:
-		animal->setSound("具克");
-		return animal;
-	case 2:
-		animal->setSound("澜皋");
-		return animal;
-	default:
-		return nullptr;
+	int random = rand() % 3;
+	srand(time(0));
+	switch(random) {
+		case 0: return new Dog();
+		case 1: return new Cat();
+		case 2: return new Cow();
 	}
 }
 
-void Animal::setSound(const char* str) {
-	if (str) {
-		strncpy_s(sound, str, sizeof(sound) - 1);
-		sound[sizeof(sound) - 1] = '\0';
-	}
-	else {
-		sound[0] = '\0';
-	}
-}
-void Animal::makeSound() {
-	std::cout << sound << std::endl;
-}
-void Zoo::addAnimal(Animal* animal) {
-	animals.push_back(animal);
+void Zoo::addAnimal(Animal* animal, int index) {
+	animals[index] = animal;
 }
 void Zoo::performActions() {
-	for (auto a : animals) {
-		if (a) {
-			a->makeSound();
-		}
-		else {
-			std::cout << "悼拱捞 绝嚼聪促." << std::endl;
+	for (int i = 0; i < 10; i++) {
+		if (animals[i]) {
+			animals[i]->makeSound();
 		}
 	}
 }
 Zoo::~Zoo() {
-	for (auto a : animals) {
-		delete a;
+	for (int i = 0; i < 10; i++) {
+		delete animals[i];
 	}
 }
